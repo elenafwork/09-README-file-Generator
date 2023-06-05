a5e7191c34e04b35d169770ec313c7607ffa221f
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-06-05 17:10:48 -0500

    license referrence

--- index.js
+++ index.js
@@ -1,14 +1,92 @@
 // TODO: Include packages needed for this application
 const fs=require('fs');
 const inquirer=require('inquirer')
+const generateMarkdown=require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+console.log('Hello. Welcome to README File Generator! ');
+console.log('Please, answer the following questions for the successful result!');
+
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'title',
+      },
+
+      {
+        type: 'input',
+        message: 'Description: please, provide the description of your project',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Installation: please, provide the information about the project installation',
+        name: 'installation',
+      },
+      
+      {
+        type: 'input',
+        message: 'Usage: please, provide the information how to use your project',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Credits: please, provide the information about the possible project contributors',
+        name: 'credits',
+      },
+      {
+        type: 'list',
+        message: 'License: please, provide the information about license',
+        choices: ['None', 'Apace License 2.0', 'GNU General Public License v3.0','MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0', 'Eclipse Public License 2.0', 'GNU Affero Public License v3.0','GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0','The Unlicense' ],
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'Contacts: please, provide your GitHub profile (required) for users to reach you',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: 'Contacts: please, provide your email address for users to reach you',
+        name: 'email',
+      }
+
+  
+];
+
+
+
+
+
+    
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) =>{
+        error ? console.error(error)
+        : console.log('Success!');
+        
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+.prompt(questions)
+.then((data)=> {
+    console.log('Thank you!');
+    
+    console.log(data);
+    console.log(data.title);
+       
+    writeToFile("./README-sample.md", generateMarkdown(data));
+        
+     
+}) 
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,163 @@
+const licenses=[
+  { 
+    name: 'None',
+    badgeLink: ' ',
+    link: ' '
+  },
+  {
+    name: 'Apace License 2.0',
+    badgeLink: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg ',
+    link: 'https://opensource.org/licenses/Apache-2.0' 
+
+  },
+  {
+    name: 'GNU General Public License v3.0',
+    badgeLink: 'https://img.shields.io/badge/License-GPLv3-blue.svg ',
+    link: 'https://www.gnu.org/licenses/gpl-3.0' 
+
+  },
+  {
+    name: 'MIT License',
+    badgeLink: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    link: 'https://opensource.org/licenses/MIT' 
+
+  },
+  {
+    name: 'BSD 2-Clause "Simplified" License',
+    badgeLink: 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg',
+    link: 'https://opensource.org/licenses/BSD-2-Clause' 
+
+  },
+  {
+    name: 'BSD 3-Clause "New" or "Revised" License',
+    badgeLink: 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg',
+    link: 'https://opensource.org/licenses/BSD-3-Clause' 
+
+  },
+  {
+    name: 'Boost Software License 1.0',
+    badgeLink: 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg',
+    link: 'https://www.boost.org/LICENSE_1_0.txt' 
+
+  },
+  {
+    name: 'Creative Commons Zero v1.0',
+    badgeLink: 'https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg',
+    link: 'http://creativecommons.org/publicdomain/zero/1.0/' 
+
+  },
+  {
+    name: 'Eclipse Public License 2.0',
+    badgeLink: 'https://img.shields.io/badge/License-EPL_1.0-red.svg',
+    link: 'https://opensource.org/licenses/EPL-1.0' 
+
+  },
+  {
+    name: 'GNU Affero Public License v3.0',
+    badgeLink: 'https://img.shields.io/badge/License-AGPL_v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/agpl-3.0' 
+
+  },
+  {
+    name: 'GNU General Public License v2.0',
+    badgeLink: 'https://img.shields.io/badge/License-GPL_v2-blue.svg',
+    link: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html' 
+
+  },
+
+  {
+    name: 'GNU Lesser General Public License v2.1',
+    badgeLink: 'https://img.shields.io/badge/License-LGPL_v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/lgpl-3.0' 
+
+  },
+  
+  {
+    name: 'Mozilla Public License 2.0',
+    badgeLink: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    link: 'https://opensource.org/licenses/MPL-2.0' 
+
+  },
+  
+  {
+    name: 'The Unlicense',
+    badgeLink: 'https://img.shields.io/badge/license-Unlicense-blue.svg',
+    link: 'http://www.wtfpl.net/about/' 
+
+  }
+]
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  for(const element of licenses ){
+    if(element.name==license){
+      console.log(element.badgeLink);
+      
+      return element.badgeLink
+    }
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  for(const element of licenses ){
+    if(element.name==license){
+      console.log(element.link);
+      return element.link
+    }
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  renderLicenseBadge(license);
+  renderLicenseLink(license);
+  console.log(` [![License](${ renderLicenseBadge(license)}) ](${renderLicenseLink(license)}) `);
+  
+  return `[![License](${ renderLicenseBadge(license)}) ](${renderLicenseLink(license)})`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data);
+  console.log(data.license);
+  
+  //renderLicenseSection(data)
+  
+  
+  return `
+  # ${data.title}
+
+  ## Licenses
+  ${renderLicenseSection(data.license)}
+
+  ## Table of Content
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage 
+  ${data.usage}
+  ## Credits
+  ${data.credits}
+ 
+  ## Questions
+  If you have any questions regarding the project, please, feel free to contact me:
+  - GitHub: [${data.github}](https://github/${data.github})
+  - Email: <${data.email}>
+
 
 `;
 }
